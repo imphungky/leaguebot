53df5b4f52e6930421a13d2e4099534c240ddf19
imphungky <phungky321@gmail.com>
2021-01-27 04:00:42 -0500

    added command to retrieve matches from match history

--- championFetch.js
+++ championFetch.js
@@ -1,5 +1,13 @@
+const { default: axios } = require('axios');
 const axiosConfig = require('./axiosConfig.js');
 const {regions} = require('./config.json');
+
+function SStoMS(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    return {minutes: minutes, seconds: seconds - minutes * 60};
+}
+
+
 async function getPatch() {
     return axiosConfig.get(`http://ddragon.leagueoflegends.com/api/versions.json`)
     .then((response) => {
@@ -31,7 +39,7 @@ async function getchampmastery(uid, max) {
     });
 }
 
-async function champIDtoName(id) {
+async function champNametoID(name, patch) {
     return axiosConfig.get(`http://ddragon.leagueoflegends.com/cdn/11.2.1/data/en_US/champion.json`)
     .then(response => {
         const champlist = response.data.data;
@@ -46,10 +54,68 @@ async function champIDtoName(id) {
     });
 }
 
+async function champIDtoName(id, patch) {
+    return axiosConfig.get(`http://ddragon.leagueoflegends.com/cdn/${patch}/data/en_US/champion.json`)
+    .then(response => {
+        const champlist = response.data.data;
+        let name = Object.values(champlist).filter(champion => champion.key == id);
+        if(name.length) {
+            return {name: name[0].name, image: name[0].image.full, title: name[0].title};
+        }
+        return 'error';
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+async function getMatchHistory(uid) {
+    return axiosConfig.get(`${regions.NA}/lol/match/v4/matchlists/by-account/${uid}`)
+    .then(response => {
+        return response.data;
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+async function getMatchDetails(matchid, username) {
+
+    /**
+     * End point returns data object on success
+     * Fields that are relevant are: 
+     *     participantIdentities - [Objects] return player_id if player.accountId == ours
+     *     participants - after retrieving participant ID we match it within this list to get details
+     *     gameDuration - 
+     *     We won't be able to display the items currently so the only relevant fields we need from the returned object is:
+     *         stats.kills, stats.deaths, stats.assists, stats.win, 
+     */
+
+
+    return axiosConfig.get(`${regions.NA}/lol/match/v4/matches/${matchid}`)
+    .then((response) => {
+        const match_info = response.data;
+        let gameDuration = SStoMS(match_info.gameDuration);
+        let [player] = match_info.participantIdentities.filter((participant) => participant.player.summonerName == username);
+        let [player_details] = match_info.participants.filter((participant) => participant.participantId == player.participantId);
+        return {
+            win: player_details.stats.win, 
+            kills: player_details.stats.kills,
+            assists: player_details.stats.assists, 
+            deaths: player_details.stats.deaths,
+            length: gameDuration
+        };
+        
+    })
+}
+
 module.exports = {
     getPatch: getPatch,
     getuserInfo: getuserInfo,
     getchampmastery: getchampmastery,
-    champIDtoName: champIDtoName
+    champIDtoName: champIDtoName,
+    champNametoID: champNametoID,
+    getMatchHistory: getMatchHistory,
+    getMatchDetails: getMatchDetails
 };
 
--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const {
     getPatch,
     getuserInfo,
     getchampmastery,
-    champIDtoName
+    champIDtoName,
+    getMatchHistory,
+    getMatchDetails
 } = require('./championFetch.js');
 const client = new Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
@@ -21,6 +23,7 @@ client.on('message', async message =>  {
         //split message as a space as delimiter
         let args = message.content.slice(prefix.length).trim().split(/ +/);
         let command = args.shift().toLowerCase();
+        let patch;
         switch(command) {
             case "getinfo":
                 let summoner_name = args[0];
@@ -53,6 +56,7 @@ client.on('message', async message =>  {
                     return message.channel.send("Usage: !championmastery <summonername> <champion>")
                 }
                 let user_icon = "";
+                patch = await getPatch();
                 getuserInfo(args[0])
                 .then((user_data) => {
                     user_icon = user_data.profileIconId;
@@ -60,7 +64,7 @@ client.on('message', async message =>  {
                 })
                 .then(async (masteries) => {
                     let namepromises = masteries.map((mastery) => {
-                        return champIDtoName(mastery.championId);
+                        return champIDtoName(mastery.championId, patch.patch);
                     });
                     let names = await Promise.all(namepromises);
                     return masteries.map((mastery, index) => {return {...mastery, ...names[index]}});
@@ -71,20 +75,56 @@ client.on('message', async message =>  {
                         .setColor('#0099ff')
                         .setTitle(`${args[0]} Top Champion Masteries`)
                         .setURL('https://discord.js.org/')
-                        .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/11.2.1/img/profileicon/${user_icon}.png`)
-                        .setImage(`http://ddragon.leagueoflegends.com/cdn/11.2.1/img/champion/${masteries[i].image}`)
+                        .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/${patch.patch}/img/profileicon/${user_icon}.png`)
+                        .setImage(`http://ddragon.leagueoflegends.com/cdn/${patch.patch}/img/champion/${masteries[i].image}`)
                         .addFields(
-                            { name: masteries[i].name, value: masteries[i].title },
+                            { name: masteries[i].name, value: masteries[i].title},
                             // { name: '\u200B', value: '\u200B' },
-                            { name: 'Champion Mastery', value: masteries[i].championPoints, inline: true },
-                            { name: 'Champion Level', value: masteries[i].championLevel, inline: true },
+                            { name: 'Champion Mastery', value: masteries[i].championPoints},
                         )
                         .setFooter(`Mastery Level ${masteries[i].championLevel}` , `https://raw.githubusercontent.com/RiotAPI/Riot-Games-API-Developer-Assets/master/champion-mastery-icons/mastery-${masteries[i].championLevel}.png`);
                         message.channel.send(exampleEmbed);
                     }
                 })
                 .catch((err) => console.log(err));
-                
+                break;
+            case "matches":
+                if(!args.length || args.length > 1) {
+                    return message.channel.send('USAGE: !match <summoner>');
+                }
+                patch = await getPatch();
+                getuserInfo(args[0])
+                .then((user_data) => {
+                    console.log(user_data.accountId);
+                    return getMatchHistory(user_data.accountId);
+                })
+                .then(async (data) => {
+                    let championNamesPromise = data.matches.slice(0, 3).map((match) => champIDtoName(match.champion, patch.patch));
+                    let matchdetailsPromise = data.matches.slice(0, 3).map((match) => getMatchDetails(match.gameId, args[0]));
+                    let championNames = await Promise.all(championNamesPromise);
+                    let matchdetails = await Promise.all(matchdetailsPromise);
+                    return data.matches.slice(0, 3).map((match, index) => {return {...match, ...matchdetails[index], ...championNames[index]}});
+                })
+                .then((match_history) => {
+                    for(let i = 0; i < match_history.length; i++) {
+                        console.log(match_history[i]);
+                        const exampleEmbed = new MessageEmbed()
+                        .setColor(match_history[i].win ? '#00ed28' : '#d90000')
+                        .setTitle(match_history[i].win ? 'Victory' : 'Loss')
+                        .setURL('https://discord.js.org/')
+                        .setImage(`http://ddragon.leagueoflegends.com/cdn/${patch.patch}/img/champion/${match_history[i].image}`)
+                        .addFields(
+                            // { name: '\u200B', value: '\u200B' },
+                            { name: 'ROLE', value: match_history[i].lane, inline: true},
+                            { name: 'K/D/A', value: `${match_history[i].kills}/${match_history[i].deaths}/${match_history[i].assists}`, inline:true},
+                            { name: 'Game Duration', value: `${match_history[i].length.minutes}:${match_history[i].length.seconds}`, inline:true}
+                        );
+                        message.channel.send(exampleEmbed);
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         }
 });
 
